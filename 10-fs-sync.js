99912c24be34e9c6944a9f9f67518d376069313e
nimesh <2018cs131@stu.ucsc.cmb.ac.lk>
2021-08-22 20:05:01 +0530

    x

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,6 +1,10 @@
 const { readFileSync, writeFileSync } = require('fs')
+console.log('Start')
 const first = readFileSync('./content/first.txt','utf8')
 const second = readFileSync('./content/second.txt','utf8')
 
 // writeFileSync('./content/result-sync.txt',`Here is the result : ${first}, ${second}`, {flag: 'a'})
 writeFileSync('./content/result-sync.txt','hey hey', {flag: 'a'})
+
+console.log('done with this task')
+console.log('starting the next one')
