9b3c70e4e200c2b6ff4af6e78592d3f85dde0cbe
nimesh <2018cs131@stu.ucsc.cmb.ac.lk>
2021-08-23 08:40:33 +0530

    x2

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-const http = require('http')
+const _ = require('lodash')
 
-const server = http.createServer((req, res)=>{
-  if(req.url === '/'){
-    res.end('Welcome to our home page')
-  }
-  if(req.url === '/about'){
-    res.end('Here is our short history')
-  }
-  res.end(
-    `<h1>Oops!</h1>
-    <p>We can't seem to find the page</p>
-    <a href="/">BACK HOME</a>
-    `
-  )
-})
-
-server.listen(5000)
+const items = [1, [2, [3, [4, 5]]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems)
