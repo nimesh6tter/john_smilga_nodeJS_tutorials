47b197363303041fbf970af03fbc44379e98bbe2
nimesh <2018cs131@stu.ucsc.cmb.ac.lk>
2021-08-27 09:26:31 +0530

    x4

---
+++ Async-awaitES6 -in10min/1.js
@@ -0,0 +1,11 @@
+//callback hell
+
+setTimeout(() => {
+    console.log('3')
+    setTimeout(() => {
+        console.log('2')
+        setTimeout(() => {
+            console.log('1')
+        }, 1000);
+    }, 1000);
+}, 1000);
---
+++ Async-awaitES6 -in10min/2.js
@@ -0,0 +1,6 @@
+//button event handler in browser javascript is also have callback function
+
+const button
+btn.addEventListener('click', ()=>{
+    
+})
